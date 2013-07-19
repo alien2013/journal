fb4ca66ea68f1338d6525ae150d4b626115679ed
alien2013 <alien@zombiekalypse.de>
2013-07-19 20:55:12 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Journal::Application.config.secret_token = 'aef27a168218e40d56d1cf499e1c24b811a8edfdb41cc84d97e0c15ae595be0da8b0cc33caba206f780d47860c41134453514c3765a6e3f08093110ca658a389'
